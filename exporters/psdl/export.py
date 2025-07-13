5a8ad3667cde0babacb353d834d4f273c6805054
wilkovatch <29335145+wilkovatch@users.noreply.github.com>
2025-07-13 19:13:25 +0200

    export flags fixes

--- exporters/psdl/export.py
+++ exporters/psdl/export.py
@@ -10,8 +10,8 @@ from scene_input import StandaloneSceneInput
 from common.main_writer import MainWriter
 
 try:
-    if len(sys.argv) != 15:
-        raise Exception("Wrong number of arguments, must be 14 (modules path, input json file, output psdl file, and 11 flags), got " + str(len(sys.argv) - 1))
+    if len(sys.argv) != 14:
+        raise Exception("Wrong number of arguments, must be 13 (modules path, input json file, output psdl file, and 11 flags), got " + str(len(sys.argv) - 1))
     # flag order (values are 0 or 1, first index is 4):
     # - write prop rules
     # - write bin only (will ignore the subsequent flags if set)
@@ -31,7 +31,7 @@ try:
     # export BIN
     dj = DecodeExportedJson.DecodeExportedJson()
     data = dj.decode_json(sys.argv[2])
-    exp = BINExporter(data, int(sys.argv[14]) > 0)
+    exp = BINExporter(data, int(sys.argv[13]) > 0)
     exp.export_bin_file(bin_file, int(sys.argv[4]) > 0)
 
     if not int(sys.argv[5]):
@@ -41,7 +41,7 @@ try:
             psdl_file, scene_input,
             int(sys.argv[6]), int(sys.argv[7]), int(sys.argv[8]),
             0, int(sys.argv[9]), int(sys.argv[10]), int(sys.argv[11]),
-            int(sys.argv[12]))
+            int(sys.argv[12])
         )
         writer.write()
         os.remove(bin_file)
